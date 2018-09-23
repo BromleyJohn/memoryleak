3ec9d60e5260074b618b57430d696ecbe8c8c4a4
JohnAH <heria3@aol.com>
2018-09-23 12:28:50 +0100

    Changed app.js port from 3000 to 80

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 
 app.use(express.static(__dirname+'/public'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(80, () => console.log('app listening on port 80'))
